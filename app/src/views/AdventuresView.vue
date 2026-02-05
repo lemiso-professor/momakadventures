6d383d8fdfd266f86cd617ef477e17cd28bacae7
lemiso-professor <mathiaskipkoech45@gmail.com>
2026-02-05 22:37:51 +0300

    Update imports to match PascalCase filenames

--- app/src/views/AdventuresView.vue
+++ app/src/views/AdventuresView.vue
@@ -9,8 +9,8 @@
 <script setup>
 
 import Adventures from '../components/adventures/Adventures.vue';
-import FilterAd from '../components/adventures/Filterad.vue';
-import Mountain from '../components/adventures/mountain.vue';
+import FilterAd from '../components/adventures/FilterAd.vue';
+import Mountain from '../components/adventures/Mountain.vue';
 
 </script>
 
